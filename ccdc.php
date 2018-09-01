efcf149823e7ac7d3c91257cbde01bd7ee3fb1d7
Micah Raney <MicahRaney@users.noreply.github.com>
2018-09-01 13:57:13 -0700

    Added CCDC page

---
+++ ccdc.php
@@ -0,0 +1,60 @@
+<html>
+<title>Cyber @ UCI | Calendar</title>
+<head>
+<?php include('common/head.html');?>
+</head>
+<?php include('common/navbar.html');?>
+<body class="w3-black">
+<div class="w3-container w3-content w3-center w3-padding-64">
+	<div class="w3-container w3-content w3-padding-32" style="max-width:800px">
+		<h2 class="w3-wide w3-jumbo w3-center">CCDC</h2>
+		<p class="w3-opacity w3-center"><i>Where cybersecurity gets real.</i></p><br>
+
+		<h3 class="w3-center">
+		What is it?
+		</h3>
+		<p class="w3-justify">
+			Collegiate Cyber Defense Competition is where you take the skills you learn to the next level:
+			defending a small business network in real time from trained cybersecurity experts. A true test
+			of grit and knowledge, CCDC has a reputation of being highly demanding, yet also highly rewarding.
+		</p>
+
+		<br><br>
+		<h3 class="w3-center">
+		What does joining look like?
+		</h3>
+		<p class="w3-justify">
+			Joining CCDC means joining a team of highly motivated individuals who meet every week to
+			learn about new defense techniques, common security mistakes, and proper setup and configuration.
+		</p>
+
+
+
+		<br><br>
+		<h3 class="w3-center">
+		I'm new!
+		</h3>
+		<p class="w3-justify">
+			While sometimes overwhelming, CCDC is a great place to jump in for beginners as well, since
+			the team structure is meant to facilitate cooperative learning and many CCDC veterans area
+			happy to help explain
+		</p>
+
+
+		<br><br>
+		<h3 class="w3-center">
+		What Next?
+		</h3>
+		<a href="/slack" class="w3-button w3-white w3-text-black w3-padding-large w3-large w3-margin-top w3-transition-medium">
+				Join #ccdc on Slack
+		</a>
+	</div>
+
+</div>
+</body>
+<!-- Footer -->
+<footer class="w3-container w3-padding w3-center w3-opacity w3-light-grey w3-xlarge">
+	<?php include('common/social.html');?>
+	<?php include('common/legal.html');?>
+</footer>
+</html>
