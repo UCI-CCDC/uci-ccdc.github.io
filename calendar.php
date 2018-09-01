35e9f5e7bd2808a138f72c60e05f1c07dede1fb7
Micah Raney <MicahRaney@users.noreply.github.com>
2018-09-01 13:19:58 -0700

    Restyled calendar

--- calendar.php
+++ calendar.php
@@ -5,12 +5,9 @@
 </head>
 <?php include('common/navbar.html');?>
 
+<div class="w3-container w3-content w3-center w3-padding-64">
+<h2 class="w3-wide">CLUB CALENDAR</h2>
 <br>
-<br>
-<br>
-<h2 class="w3-center">Cyber @ UCI Calendar</h2>
-<br>
-<div class="w3-center w3-padding">
 <iframe src="https://calendar.google.com/calendar/embed?src=jrqjgk7bojll3fcltv9h94nhls%40group.calendar.google.com&ctz=America%2FLos_Angeles" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
 </div>
 <!-- Footer -->
