fbfa5cdd45352e3d3c02c93d5dffdafea093bbe8
Micah Raney <MicahRaney@users.noreply.github.com>
2018-09-01 13:45:14 -0700

    Added board page w/ template

---
+++ board.php
@@ -0,0 +1,111 @@
+<html>
+<title>Cyber @ UCI | Calendar</title>
+<head>
+<?php include('common/head.html');?>
+</head>
+<?php include('common/navbar.html');?>
+
+<div class="w3-container w3-content w3-center w3-padding-64">
+<h2 class="w3-wide">THE BOARD</h2>
+
+<!-- Board Members -->
+<div class="w3-container">
+  <div class="w3-row-padding w3-center" style="margin-top:64px">
+
+	    <div class="w3-col l4 m6 w3-margin-bottom">
+	      <div class="w3-card w3-white">
+	        <img src="/images/board/member.jpg" alt="Profile" style="width:100%">
+	        <div class="w3-container"><div class="autoflow" autoflowid="1">
+	          <h3>Board Member</h3>
+	          <div class="w3-large">Role</div>
+	          <p>
+	            Something bio something
+	          </p>
+					</div><p><a href="" class="w3-button w3-light-grey w3-block"><i class="fa fa-envelope"></i> Contact</a></p>
+	        </div>
+	      </div>
+	    </div>
+
+	    <div class="w3-col l4 m6 w3-margin-bottom">
+	      <div class="w3-card w3-white">
+	        <img src="/images/board/member.jpg" alt="Profile" style="width:100%">
+	        <div class="w3-container"><div class="autoflow" autoflowid="1">
+	          <h3>Board Member</h3>
+	          <div class="w3-large">Role</div>
+	          <p>
+	            Something bio something
+	          </p>
+					</div><p><a href="" class="w3-button w3-light-grey w3-block"><i class="fa fa-envelope"></i> Contact</a></p>
+	        </div>
+	      </div>
+	    </div>
+
+	    <div class="w3-col l4 m6 w3-margin-bottom">
+	      <div class="w3-card w3-white">
+	        <img src="/images/board/member.jpg" alt="Profile" style="width:100%">
+	        <div class="w3-container"><div class="autoflow" autoflowid="1">
+	          <h3>Board Member</h3>
+	          <div class="w3-large">Role</div>
+	          <p>
+	            Something bio something
+	          </p>
+					</div><p><a href="" class="w3-button w3-light-grey w3-block"><i class="fa fa-envelope"></i> Contact</a></p>
+	        </div>
+	      </div>
+	    </div>
+
+			<!-- Spacer Div-->
+	    <div class="w3-col l2 w3-margin-bottom w3-hide-small w3-hide-medium">
+
+	    </div>
+
+	    <div class="w3-col l4 m6 w3-margin-bottom">
+			      <div class="w3-card w3-white">
+			        <img src="/images/board/member.jpg" alt="Profile" style="width:100%">
+			        <div class="w3-container"><div class="autoflow" autoflowid="1">
+			          <h3>Board Member</h3>
+			          <div class="w3-large">Role</div>
+			          <p>
+			            Something bio something
+			          </p>
+							</div><p><a href="" class="w3-button w3-light-grey w3-block"><i class="fa fa-envelope"></i> Contact</a></p>
+			        </div>
+			      </div>
+			    </div>
+
+			<!-- Spacer Div-->
+	    <div class="w3-col m3 w3-margin-bottom w3-hide-small w3-hide-large">
+
+	    </div>
+
+	    <div class="w3-col l4 m6 w3-margin-bottom">
+					      <div class="w3-card w3-white">
+					        <img src="/images/board/member.jpg" alt="Profile" style="width:100%">
+					        <div class="w3-container"><div class="autoflow" autoflowid="1">
+					          <h3>Board Member</h3>
+					          <div class="w3-large">Role</div>
+					          <p>
+					            Something bio something
+					          </p>
+									</div><p><a href="" class="w3-button w3-light-grey w3-block"><i class="fa fa-envelope"></i> Contact</a></p>
+					        </div>
+					      </div>
+					    </div>
+
+  </div>
+</div>
+
+
+
+</div>
+<!--Autoflow the needed divs-->
+<script src="/assets/js/autoflow.js"></script>
+<script>
+  autoflow();
+</script>
+<!-- Footer -->
+<footer class="w3-container w3-padding w3-center w3-opacity w3-light-grey w3-xlarge">
+	<?php include('common/social.html');?>
+	<?php include('common/legal.html');?>
+</footer>
+</html>
