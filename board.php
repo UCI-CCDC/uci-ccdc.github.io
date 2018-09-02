4f9001b5318b1be787d5b0c9944d14771391dfa6
Micah Raney <MicahRaney@users.noreply.github.com>
2018-09-01 21:37:11 -0700

    Added board page and info

--- board.php
+++ board.php
@@ -6,102 +6,73 @@
 <?php include('common/navbar.html');?>
 
 <div class="w3-container w3-content w3-center w3-padding-64">
-<h2 class="w3-wide">THE BOARD</h2>
+	<h2 class="w3-wide">THE BOARD</h2>
 
-<!-- Board Members -->
-<div class="w3-container">
-  <div class="w3-row-padding w3-center" style="margin-top:64px">
-
-	    <div class="w3-col l4 m6 w3-margin-bottom">
-	      <div class="w3-card w3-white">
-	        <img src="/images/board/member.jpg" alt="Profile" style="width:100%">
-	        <div class="w3-container"><div class="autoflow" autoflowid="1">
-	          <h3>Board Member</h3>
-	          <div class="w3-large">Role</div>
-	          <p>
-	            Something bio something
-	          </p>
-					</div><p><a href="" class="w3-button w3-light-grey w3-block"><i class="fa fa-envelope"></i> Contact</a></p>
-	        </div>
-	      </div>
-	    </div>
-
-	    <div class="w3-col l4 m6 w3-margin-bottom">
-	      <div class="w3-card w3-white">
-	        <img src="/images/board/member.jpg" alt="Profile" style="width:100%">
-	        <div class="w3-container"><div class="autoflow" autoflowid="1">
-	          <h3>Board Member</h3>
-	          <div class="w3-large">Role</div>
-	          <p>
-	            Something bio something
-	          </p>
-					</div><p><a href="" class="w3-button w3-light-grey w3-block"><i class="fa fa-envelope"></i> Contact</a></p>
-	        </div>
-	      </div>
-	    </div>
-
-	    <div class="w3-col l4 m6 w3-margin-bottom">
-	      <div class="w3-card w3-white">
-	        <img src="/images/board/member.jpg" alt="Profile" style="width:100%">
-	        <div class="w3-container"><div class="autoflow" autoflowid="1">
-	          <h3>Board Member</h3>
-	          <div class="w3-large">Role</div>
-	          <p>
-	            Something bio something
-	          </p>
-					</div><p><a href="" class="w3-button w3-light-grey w3-block"><i class="fa fa-envelope"></i> Contact</a></p>
-	        </div>
-	      </div>
-	    </div>
-
-			<!-- Spacer Div-->
-	    <div class="w3-col l2 w3-margin-bottom w3-hide-small w3-hide-medium">
-
-	    </div>
-
-	    <div class="w3-col l4 m6 w3-margin-bottom">
-			      <div class="w3-card w3-white">
-			        <img src="/images/board/member.jpg" alt="Profile" style="width:100%">
-			        <div class="w3-container"><div class="autoflow" autoflowid="1">
-			          <h3>Board Member</h3>
-			          <div class="w3-large">Role</div>
-			          <p>
-			            Something bio something
-			          </p>
-							</div><p><a href="" class="w3-button w3-light-grey w3-block"><i class="fa fa-envelope"></i> Contact</a></p>
-			        </div>
-			      </div>
-			    </div>
-
-			<!-- Spacer Div-->
-	    <div class="w3-col m3 w3-margin-bottom w3-hide-small w3-hide-large">
-
-	    </div>
-
-	    <div class="w3-col l4 m6 w3-margin-bottom">
-					      <div class="w3-card w3-white">
-					        <img src="/images/board/member.jpg" alt="Profile" style="width:100%">
-					        <div class="w3-container"><div class="autoflow" autoflowid="1">
-					          <h3>Board Member</h3>
-					          <div class="w3-large">Role</div>
-					          <p>
-					            Something bio something
-					          </p>
-									</div><p><a href="" class="w3-button w3-light-grey w3-block"><i class="fa fa-envelope"></i> Contact</a></p>
-					        </div>
-					      </div>
-					    </div>
-
-  </div>
-</div>
+	<div class="w3-row-padding" style="margin-top:64px">
 
 
-
+		<div class="w3-col l4 m6 w3-margin-bottom">
+			<div class="w3-card w3-white">
+				<img src="/images/board/jacob.jpg" alt="Member" style="width:100%">
+				<div class="w3-container">
+					<h3>Jacob Sigismonti</h3>
+					<p>President</p>
+					<p><a href="mailto:jsigismo@uci.edu" class="w3-button w3-light-grey w3-block"><i class="fa fa-envelope"></i> Contact</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="w3-col l4 m6 w3-margin-bottom">
+			<div class="w3-card w3-white">
+				<img src="/images/board/alvin.jpg" alt="Member" style="width:100%">
+				<div class="w3-container">
+					<h3>Alvin Nguyen</h3>
+					<p>External VP</p>
+					<p><a href="mailto:alvindn2@uci.edu" class="w3-button w3-light-grey w3-block"><i class="fa fa-envelope"></i> Contact</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="w3-col l4 m6 w3-margin-bottom">
+			<div class="w3-card w3-white">
+				<img src="/images/board/tommy.jpg" alt="Member" style="width:100%">
+				<div class="w3-container">
+					<h3>Tommy Le</h3>
+					<p>Internal VP</p>
+					<p><a href="mailto:tommyll@uci.edu" class="w3-button w3-light-grey w3-block"><i class="fa fa-envelope"></i> Contact</a></p>
+				</div>
+			</div>
+		</div>
+		<!-- Spacing Div-->
+		<div class="w3-col l2 w3-margin-bottom w3-hide-medium w3-hide-small">
+		</div>
+		<div class="w3-col l4 m6 w3-margin-bottom">
+			<div class="w3-card w3-white">
+				<img src="/images/board/hassan.jpg" alt="Member" style="width:100%">
+				<div class="w3-container">
+					<h3>Hassan Khokhar</h3>
+					<p>Marketing</p>
+					<p><a href="mailto:hkhokhar@uci.edu" class="w3-button w3-light-grey w3-block"><i class="fa fa-envelope"></i> Contact</a></p>
+				</div>
+			</div>
+		</div>
+		<!-- Spacing Div-->
+		<div class="w3-col m3 w3-margin-bottom w3-hide-large w3-hide-small">
+		</div>
+		<div class="w3-col l4 m6 w3-margin-bottom">
+			<div class="w3-card w3-white">
+				<img src="/images/board/micah.jpg" alt="Member" style="width:100%">
+				<div class="w3-container">
+					<h3>Micah Raney</h3>
+					<p>Treasurer & Corporate Outreach</p>
+					<p><a href="mailto:inkiad@zotbotics.org" class="w3-button w3-light-grey w3-block"><i class="fa fa-envelope"></i> Contact</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 <!--Autoflow the needed divs-->
 <script src="/assets/js/autoflow.js"></script>
 <script>
-  autoflow();
+  //autoflow();
 </script>
 <!-- Footer -->
 <footer class="w3-container w3-padding w3-center w3-opacity w3-light-grey w3-xlarge">
