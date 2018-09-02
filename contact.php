1f1442b31872433189e9c0afae7fc2460060fa7b
Micah Raney <raneym@uci.edu>
2018-09-02 00:28:35 -0700

    Textual clarification

--- contact.php
+++ contact.php
@@ -47,7 +47,7 @@ if(!isset($_POST['name']) ||
 
   //set up email content
   $to = "ucicyber@gmail.com";
-  $subject = "[WEBSITE CONTACT] Message from  $name ($email)";
+  $subject = "[WEBSITE] Message from  $name ($email)";
   $headers = "Reply-to: $email";
   $message = "Message from  $name ($email) \r\n";
   $message .= "UTC Timestamp: ". gmdate("M d Y H:i:s", time());
